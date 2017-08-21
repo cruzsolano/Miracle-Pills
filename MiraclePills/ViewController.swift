f9a327d0da6acfbe52af55e5729bc1c6af625794
cruzsolano <30808112+cruzsolano@users.noreply.github.com>
2017-08-21 15:59:27 -0700

    Added a functional View Picker

--- MiraclePills/ViewController.swift
+++ MiraclePills/ViewController.swift
@@ -8,10 +8,22 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
 
+    @IBOutlet weak var statePicker: UIPickerView!
+    @IBOutlet weak var statePickerButton: UIButton!
+    @IBOutlet weak var country: UILabel!
+    @IBOutlet weak var enterCountry: UITextField!
+    @IBOutlet weak var zipCode: UILabel!
+    @IBOutlet weak var enterZipCode: UITextField!
+    
+    
+    let states = ["Alaska", "Alabama", "Arkansas", "Oregon", "California", "New York", "Maine", "Idaho", "Washington"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        statePicker.dataSource = self
+        statePicker.delegate = self
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -20,6 +32,38 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func stateButtonPressed(_ sender: Any) {
+
+        statePicker.isHidden = false
+        country.isHidden = true
+        enterCountry.isHidden = true
+        zipCode.isHidden = true
+        enterZipCode.isHidden = true
+        
+    }
+   
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return states.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return states[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        
+        statePickerButton.setTitle(states[row], for: UIControlState.normal)
+        statePicker.isHidden = true
+        country.isHidden = false
+        enterCountry.isHidden = false
+        zipCode.isHidden = false
+        enterZipCode.isHidden = false
+    }
 
 }
 
